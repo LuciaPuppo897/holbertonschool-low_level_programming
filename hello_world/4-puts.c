7b3f3d8647cba1aeb85262d87d37aa4f32470e11
root <alerome897@gmail.com>
2023-06-14 11:34:46 -0700

    betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line. 
+ * main - prints "Programming is like building a multilingual puzzle, followed by a new line." 
  * Return always 0 (success)
  */
 int main(void)
