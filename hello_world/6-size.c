562aca2a87d538ca042ebec4567c8ae1c0466265
root <alerome897@gmail.com>
2023-06-15 06:23:55 -0700

    size of differents types in byte(S)

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
+
 /**
- * main = Entry point
+ * main - Entry point
  *
- * Return= always 0 (success)
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
+	int a;
+	long int b;
+	long long int c;
+	char d;
 	float f;
-	long int a;
-	char c;
-        long long int b;
-	/**sizeof evaluates the size of a variable*/
-printf("size of a char: %lu byte(s)\n", sizeof(c));
-printf("size of an int: %lu byte(s)\n", sizeof(i));
-printf("size of a long int: %lu byte(s)\n", sizeof(a));
-printf("size of  a float: %lu byte(s)\n", sizeof(f));
-printf("size of a long long int: %lu byte(s)\n", sizeof(b));
 
-return (0);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
