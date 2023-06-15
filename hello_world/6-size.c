c83b679b6e70ea048c717859262d6908d8d023bf
root <alerome897@gmail.com>
2023-06-15 06:00:54 -0700

    size of diferents types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int intType;
 	float floatType;
-	double doubleType;
+	long int longinttype;
 	char charType;
-
+        long long int; longlonginttype
 	/**sizeof evaluates the size of a variable*/
-	printf("size of int: %zu bytes\n", sizeof(intType));
-	printf("size of float: %zu bytes\n", sizeof(floatType));
-	printf("size of double: %zu bytes\n", sizeof(doubleType));
-	printf("size of char: %zu bytes\n", sizeof(charType));
-
-	return (0);
+	printf("size of a char: %lu bytes\n",(unsigned long) sizeof(charType));
+	printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(intType));
+	printf("size of a long int: %lu bytes\n",(unsigned long)sizeof(longintType));
+        printf("size of float: %lu byte(s)\n",(unsigned long)sizeof(floatType));
+        printf("size of a long long int: %lu bytes\n",(unsigned long)sizeof(longlonginttype));
+        return (0);
 }
