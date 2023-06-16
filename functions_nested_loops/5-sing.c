5a3d0cbf58a8b576b2260fa727d83beeaa9689b5
root <alerome897@gmail.com>
2023-06-16 11:00:59 -0700

    grater or lower

---
+++ functions_nested_loops/5-sing.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * _printsing - Checks if less or greater than 0
+  * @n: The number to be checked
+  *
+  * Return: 1 for grater  0 for zero or -1 for anything else
+  */
+int print_sing(int n)
+{
+	if (n > 0)
+        {
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+        	return (-1);
+	}
+	else 
+	{
+		_putchar(0);
+		return (0);
+	}
+}
