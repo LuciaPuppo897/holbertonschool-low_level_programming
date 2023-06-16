41515381a75c6274e69a5c6bc5f1fc12d6528504
root <alerome897@gmail.com>
2023-06-16 05:41:43 -0700

    print hex in lowcase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{	int i;
+	char low;
+
+	for (i = 0; i <= 16; i++)
+		putchar(i);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
