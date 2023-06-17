05a60c68c99e3441389b8cb5947299e0e2953f4b
root <alerome897@gmail.com>
2023-06-17 14:01:27 -0700

    naturals numbers util 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,13 +3,13 @@
 /**
  *print_to_98 - print n until 98
  *@n: number
- *
+ *@i: contador
  */
 void print_to_98(int n)
 {
 	for (i = n ; i <= 98; i++)
 	{
-		_printf(i);
+		printf(i);
 		_putchar(',');
 		_putchar(' ');
 		_putchar('\n');
