8d99655f81a8e50138a98e334732b86b32bd12b3
root <alerome897@gmail.com>
2023-06-18 21:22:11 -0700

    correct 11 n until 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,17 +3,22 @@
 /**
  *print_to_98 - print n until 98
  *@n: number
- *@i: contador
+ *
  */
 void print_to_98(int n)
 {
 	int i = n;
 
-	for (; i <= 98; i++)
-	{
-		printf("%d ", i);
-		_putchar(',');
-		_putchar(' ');
-		_putchar('\n');
-	}
+	if (i <= 98)
+{
+		while (i < 98)
+		printf("%d, ", i++);
+		printf("%d\n", i);
+}
+	else
+{
+	while (n < 98)
+		printf("%d, ", i--);
+	printf("%d\n", i);
+}
 }
