44240c6f56db9f4d53b0f6aa5460fb6674abd4c3
root <alerome897@gmail.com>
2023-06-19 15:57:46 -0700

    4 print more numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - print numbers from 0 to 9 except 2 or 4
+ *@c: value
+ *
+ *Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	char c = '0';
+
+		while (c <= '9')
+{
+			if (c != 2 || c != 4)
+{
+				putchar(c);
+
+}
+		c++;
+}
+putchar('\n');
+}
