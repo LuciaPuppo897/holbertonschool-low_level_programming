1441cdf3ca8835e6ae56c3b72d8b27022d82a104
root <alerome897@gmail.com>
2023-06-19 11:04:09 -0700

    1 is a digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /** 
- * isdigit - check if int is a digit
+ * _isdigit - check if int is a digit
  * @c: the int to check
  *
  * Return: 1 for digit or 0 if else
@@ -8,7 +8,7 @@
 
 int _isdigit(int c)
 {
-	if (c > 0 && c < 9)
+	if (c >= 0 && c =< 9)
 	{
 		return (1);
 	}
