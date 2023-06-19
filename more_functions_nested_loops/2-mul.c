e6b68b9a7881e63abfd5275b090e6019939d5363
root <alerome897@gmail.com>
2023-06-19 15:04:16 -0700

    2 multiplie a*b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+ #include "main.h"
+/**
+ * mul - mutiplies 2 numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: the result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
