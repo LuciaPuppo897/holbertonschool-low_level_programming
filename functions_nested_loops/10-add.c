f83bfeaafcffa9b9515eb9f415d3a178e5133069
root <alerome897@gmail.com>
2023-06-19 06:20:48 -0700

    10 add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,7 +3,7 @@
  * add - sum two values and return the result
  * @inta: the first value to compute
  * @intb: the second value to compute
- *
+ * Return: return value
  */
 int add(int a, int b)
 {
