9dca3bbd40b0b9236ad36ff7b42ad7c6356960dd
root <alerome897@gmail.com>
2023-06-20 03:16:21 -0700

    8 print a square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -14,7 +14,7 @@ void print_square(int size)
 
 	if (size <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
@@ -22,10 +22,10 @@ void print_square(int size)
 		{
 			for (y = 0; y < size; y++)
 			{
-				_putchar(35);
+				putchar(35);
 			}
 
-			_putchar('\n');
+			putchar('\n');
 		}
 	}
 }
