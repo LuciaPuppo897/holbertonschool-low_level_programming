993957e7648d6d13ed50dff26e6b933aeda6d1c5
root <alerome897@gmail.com>
2023-06-21 15:12:10 -0700

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * reset_to_98 - Update n to 98 
- * @*n: the poitnet to n
- * 
- * Return: always 0
- */
-void reset_to_98 (int *n)
+* reset_to_98 - Update n to 98 
+* @*n: the poitnet to n
+* 
+* Return: always 0
+*/
+void reset_to_98(int *n)
 {
-	*n = 98;
+int n;
+int* n = &n 
+*n = 98;
 }
