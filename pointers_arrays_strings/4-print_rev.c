9a89a7d8d0c0843d41243cf0cfe69664ccf4ed70
root <alerome897@gmail.com>
2023-06-22 10:22:25 -0700

    4 print reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_puts - prints a string
+ *@str: string
+ *
+ *Return: void
+ */
+void _print_rev(char *s)
+{
+	int c = 0;
+
+	  while(s[c] != 0)
+	    {
+	     c++
+	    }
+	for (c = 0; s[c]; c--)
+	{
+		_putchar(s[c] + 0);
+	}
+_putchar('\n');
+}
