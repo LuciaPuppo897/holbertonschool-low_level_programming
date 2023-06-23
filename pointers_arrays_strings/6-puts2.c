174dc1c51d9a3b385b40882f2edc8d91408fdc93
root <alerome897@gmail.com>
2023-06-23 06:54:20 -0700

    print cada 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,21 @@
 #include "main.h"
 /**
- *puts2 - print every so 2 
+ *puts2 - print every so 2
  *@str: string
  *
  *Return: void
  */
 void puts2(char *str)
-{	
+{
 	int c;
+	int count = 0;
 
-	for (c = 0; str[c] != '\0'; c += 2)
+	for (c = 0; str[c] != '\0'; c++)
+	{
+		if (c % 2 != 0)
 		_putchar(str[c]);
-
+		count++;
+	}
 _putchar('\n');
 }
 
