7890f3f44f80a37b9a14489fb84ce92537574103
root <alerome897@gmail.com>
2023-06-23 09:36:23 -0700

    8 print n char of  an array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,9 @@ void print_array(int *a, int n)
 	int c;
 
 for (c = 0; c <= n; c++)
-	printf("%d", a[n]);
+{	printf("%d", a[n]);
 	if (c != n)
-	printf(", ");
-
+		printf(", ");
+}
 _putchar('\n');
 }
