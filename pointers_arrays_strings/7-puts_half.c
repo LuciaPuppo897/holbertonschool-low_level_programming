57f85c72510a507dc55e7e3dd2d37bebc7a492af
root <alerome897@gmail.com>
2023-06-23 07:41:51 -0700

    7 puts half of string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ *
+ *puts_half - prints half a string
+ *@str: string
+ *
+ *
+ */
+void puts_half(char *str)
+{
+	int c;
+	int size = strlen(str) -1;
+
+	for( c = size / 2; str[c] != '\0'; c++)
+	 _putchar(str[c]);
+}
