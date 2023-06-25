72fa81f960cdf8217ca380b5a996097f6dc4884f
root <alerome897@gmail.com>
2023-06-25 16:57:12 -0700

    100 converts a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,22 +10,27 @@ int _atoi(char *s)
 {
 	int sign = 1;
 	int result = 0;
-
+	int digit = 0;
 	while (*s == ' ')
-	s++;
-
-	if (*s == '-' || *s == '+')
 	{
-		if (*s == '-')
-			sign = -1;
-		s++;
-	}
-
-	while (*s >= '0' && *s <= '9')
+	if (*s >= '0' && *s <= '9')
 	{
-		result = (result * 10) + (*s - '0');
-		s++;
+		result = ((result * 10) + (s - '0'));
+		digit = 1;
 	}
-
+	else if (*s == '-' || *s == '+')
+	{
+		if (digit)
+			break;
+		if(*s == '-')
+			sign *= -1;
+	}
+	else if (digit)	
+	{
+	 break; // si ve un + o - guarda los numeros y si es negativo conviert
+		// en -1 para que luefo quede negatvo
+	}
+		s++;
+	} //si es un numero lo imprime 
 	return (sign * result);
-}
+}// si es negativo queda negativo sino positivo }
