a44ec69989c631250acc0bd4e37ad5fc3c2bfe3c
root <alerome897@gmail.com>
2023-06-26 05:55:23 -0700

    7 print half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,10 +13,10 @@ int size = 0;
 
 while (str[size] != '\0')
 		{
-		size++;
+			size++;
 		}
-	size++;
+			size++;
 	for (c = size / 2; str[c] != '\0'; c++)
-	_putchar(str[c]);
+		_putchar(str[c]);
 _putchar('\n');
 }
