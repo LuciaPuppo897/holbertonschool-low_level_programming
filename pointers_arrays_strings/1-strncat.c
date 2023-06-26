99432a73985bf64cdc5362c2faa8b529c0be41c8
root <alerome897@gmail.com>
2023-06-26 08:01:11 -0700

    1-strncat.c concatenate with n bytes

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strcat - concatenate two strings
+ * _strncat - concatenate two strings with n length
  *@dest: destination string
  *@src: origin string
+ *@n : int
  *Return: always 0
  *
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  int length = 0;
-	int i;
-	
+	int length = 0;
+	int i = 0;
+	size_t s = i;
 
 while (dest[length])
 length++;
 
-while (i < 0 && src[i] != '\0')
+while (i < n && src[i] != '\0')
 {
 	dest[length++] = src[i++];
 }
