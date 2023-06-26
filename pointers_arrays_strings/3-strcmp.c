dee0aa84079cef359ae6673ae692472d86938f54
root <alerome897@gmail.com>
2023-06-26 11:10:25 -0700

    3-strcpm

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,26 +8,8 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int size = 0;
-while (s1[size])
-{
-	size++;
-}
-while (s2[size])
-{
-	size++;
-}
-if (s1 == s2)
-{
-return (0);
-}
-else if (s1 > s2)
-{
-return (-1);
-}
-else
-{
-return (1);
+result = (s1 - s2);
+
+return(result);
 }
 
-}
