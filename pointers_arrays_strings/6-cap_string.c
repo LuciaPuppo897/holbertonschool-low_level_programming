7f366f3de263c23ddaa47b2c2868988c6d02f433
root <alerome897@gmail.com>
2023-06-26 19:19:52 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,21 +6,23 @@
  */
 char *cap_string(char *p)
 {
-	int i = 0;
+	int i= 0, k;
 	int j = 13;
 	char cs[] = {32, '\n', '\t', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 while (p[i])
 {
-	i = 0;
+	k = 0;
 
 
 		while (j < 13)
 		{
 			if ((i == 0 || p[i - 1] == cs[i]) && (p[i] >= 97 && p[i] <= 122))
 				p[i] = p[i] - 32;
-		}
+
 			i++;
+		}
+			k++;
 }
 
 	return (p);
