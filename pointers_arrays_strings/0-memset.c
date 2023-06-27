4586c350a7ffbd2a405887a4e28312e4a6f842bd
root <alerome897@gmail.com>
2023-06-27 03:51:29 -0700

    0-memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- *
  *_memcpy - fills memory
- *
- *
- *
+ *@s: pointer
+ *@b: constant
+ *@n: number of bytes
+ *Return: always 0
  */
-char *_memcpy(char *s, char *b, unsigned int n)
+char *_memcpy(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 	for (i = 0; i < n; i++)
-	{	s[i] = src
+	{	s[i] = b
 		
 	return (s);
 	}
