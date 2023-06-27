d54bb6f4c00b4dba99fd95285384c4ecb8553cd1
root <alerome897@gmail.com>
2023-06-26 18:54:50 -0700

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *cap_string - capitalize the words of a string
+ *@p : the string
+ * Return: a pointer
+ */
+char *cap_string(char *p)
+{
+	int i = 0;
+	int j = 13;
+	char cs[] = {32, '\n', '\t', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+while (p[i])
+{
+	i = 0;
+
+
+		while (i < 13)
+		{
+			if ((i == 0 || p[i - 1] == cs[i]) && (p[i] >= 97 && p[i] <= 122))
+				p[i] = p[i] - 32;
+		}
+			i++;
+}
+
+	return (p);
+}
