6465c6315980ff9cb259afc1616fab8bacdfe7cf
root <alerome897@gmail.com>
2023-06-26 19:41:53 -0700

    7-leet replace char

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+  * leet - Encodes a string into 1337
+  * @s: The string to encode
+  *
+  * Return: The encoded string
+  */
+char *leet(char *s)
+{
+	int a = 0; 
+	int b = 0; 
+	int size = 5;
+	char r[5] = {'A', 'E', 'O', 'T', 'L'};
+	char n[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (b < size)
+		{
+			if (s[a] == r[b])
+			{
+				s[a] = n[b];
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return (s);
+}
