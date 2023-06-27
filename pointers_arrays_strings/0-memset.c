bc5ba76538bae467106ab591a1f3e3574db43a9e
root <alerome897@gmail.com>
2023-06-27 03:48:28 -0700

    0-memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ *_memcpy - fills memory
+ *
+ *
+ *
+ */
+char *_memcpy(char *s, char *b, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{	s[i] = src
+		
+	return (s);
+	}
+}
