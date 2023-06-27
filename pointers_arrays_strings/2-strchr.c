29a6ea57c17a9a986921749e83c38bfa34864f6e
root <alerome897@gmail.com>
2023-06-27 06:27:48 -0700

    2-strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,17 @@ char *_strchr(char *s, char c)
 {
 
 	while (*s)
-	{	
-	if (*s == c)
-
-    return (s);
-	s++;
+	{
 	if (*s == c)
 		return (s);
-	return ('\0');
+
+	s++;
 	}
+	if (*s == c)
+
+		return (s);
+
+
+	return ('\0');
+
 }
