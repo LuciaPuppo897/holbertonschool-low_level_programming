2af1bd394f23ae0c150153b76286fc41a9a25b63
root <alerome897@gmail.com>
2023-06-28 20:48:47 -0700

    6-is_prime_number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,29 +1,31 @@
 #include "main.h"
 /**
- *isprime - check the % of the div
- *@a: the num
- *@b: the divisor
- * Return: int
- */
-int isprime(int a, int b)
+  * is_prime - Check if number is prime
+  * @n: the number
+  * @m: the iteration times
+  *
+  * Return: 1 for prime or 0 if not
+  */
+int is_prime(int n, int m)
 {
-	if (a <= 1)
+	if (n <= 1)
 		return (0);
 
-	else(b == 1)
-	{
+	if (n % m == 0 && m > 1)
+		return (0);
+
+	if ((n / m) < m)
 		return (1);
-	}
+
+	return (is_prime(n, m + 1));
 }
 /**
- *is_prime_number - check if a numbr is prime
- *@n: the given num
- *Return: 1 if prime or 0
- */
+  * is_prime_number - Returns if a number is prime
+  * @n: the number
+  *
+  * Return: integer value
+  */
 int is_prime_number(int n)
-{   int b;
-	if (n % b == 0)
-		return (0);
-	else
-		return (isprime(n, b - 1));
+{
+	return (is_prime(n, 1));
 }
