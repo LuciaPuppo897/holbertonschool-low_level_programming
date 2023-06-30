12c884372db0914d2ad926c92224e05f77735442
root <alerome897@gmail.com>
2023-06-30 09:22:03 -0700

    Exercise number 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - multiplies 2 values
+ *@argc: int 
+ *@argv: char
+ *Return: result or 1
+ */
+
+int main (int argc, char *argv)
+{
+	int mult = (argv[1] * argv[2])
+		if (argc < 2)
+		{
+		printf ("Error");
+	return (1);
+		}
+	printf('\n');
+	return (0);
+}
