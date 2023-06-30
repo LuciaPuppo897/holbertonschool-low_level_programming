956a6c0ec36264086d22cbb36f27e35eefdb4992
root <alerome897@gmail.com>
2023-06-30 06:42:08 -0700

    1-args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - print n of argument
+ *@argc: the int
+ *@argv: the char
+ *Return: always 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
