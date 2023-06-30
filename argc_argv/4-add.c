7f57836fc7010db84af9a64c6983c823a5b87f63
root <alerome897@gmail.com>
2023-06-30 10:08:48 -0700

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *main - multiplies 2 values
+ *@argc: int
+ *@argv: char
+ *Return: result or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int c;
+	int count = 0;
+
+	for (c = 0; c < argc; c++) //recorre el string
+
+		while (argc[c] => '0' && argc[c] <= '9') // mire los numeros
+			count++
+		printf(count)
+		if (argc[c]   )
+		printf("Error\n");
+	return (1);
+		}
+	return (0);
+
+
+}
