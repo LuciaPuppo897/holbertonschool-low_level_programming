355c3a51a9e84459034b6f19a5f930d84e6b3764
root <alerome897@gmail.com>
2023-06-30 07:03:09 -0700

    2-argc

---
+++ argc_argv/2-argc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - print n of argument
+ *@argc: the int
+ *@argv: the char
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int  i;
+
+	for (i = 0; i < argc ; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	printf("\n");
+	return (0);
+}
