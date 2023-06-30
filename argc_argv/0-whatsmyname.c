d0d642cad0393345c89b39268f85b58d4423b365
root <alerome897@gmail.com>
2023-06-30 06:22:23 -0700

    0-whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - prints the name of the funtion
+ *@argc: how many arguments are
+ *@argv: the string
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s ", argv[0]);
+	_putchar('\0');
+	}
+	return (0);
+}
+
