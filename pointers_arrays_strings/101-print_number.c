630215d37e5e5295e022c65dff0e2c7cd66c342c
root <alerome897@gmail.com>
2023-07-01 12:09:26 -0700

    101-print_number

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -7,11 +7,11 @@
  */
 void print_number(int n)
 {
- int result = ((n % 10) * 10)
+	int result = ((n / 10) * 10);
+
 	if (n < 0)
 	{
-		result =(result * -1);
+		result = (result * -1);
 	}
-		_putchar(n + 48);
-		return (result);
+		_putchar(result + 48);
 }
