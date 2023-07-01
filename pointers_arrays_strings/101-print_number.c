116d641005e679d84a69f4857f171e86c86551fe
root <alerome897@gmail.com>
2023-07-01 11:19:24 -0700

    101-print_number.c

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_number - prints an integer
+ *@n: int
+ *
+ *Return: 0
+ */
+void print_number(int n)
+{
+
+	if (n < 0)
+	_putchar ((n * -1) + 0);
+	else
+	_putchar(n + 0);
+}
