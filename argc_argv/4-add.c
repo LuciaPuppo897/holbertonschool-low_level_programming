537589cca7ca23ca3bc6c2cb681a7ca8379d8c6a
root <alerome897@gmail.com>
2023-07-01 13:54:21 -0700

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -18,15 +18,16 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	for (c = 0; c <= argc; c++) /*recorre*/
-
+	for (c = 1; c <= argc; c++) 
+	{/*recorre*/
+	for (a = 0; a < argc; a++)
 	{
 		if (isdigit(argv[c]) == 0) /**se fija si e sun digito y sino da error*/
 		{
 		printf("Error\n");
 		return (1);
 		}
-
+	}
 		else count += atoi(argv[c]); /** transforma en intger los char y los suma*/
 	}
 	printf("%d\n", count);
