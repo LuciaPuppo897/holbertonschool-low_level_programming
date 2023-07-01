335f2eec4fa238f812aefd8cd8e2ec2306c4fc10
root <alerome897@gmail.com>
2023-07-01 13:38:51 -0700

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 /**
- *main - multiplies 2 values
+ *main - add 2 values
  *@argc: int
  *@argv: char
  *Return: result or 1
@@ -10,10 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	int a;
 	int c;
-	int count = 0;
-
+	int count = 0; 
 	if (argc == 1)
 	{
 		printf("0\n");
@@ -21,18 +19,16 @@ int main(int argc, char *argv[])
 	}
 	for (c = 1; c < argc; c++)
 
-		{
-	for	(a = 0; a <= argc; a++)
-
-			{
+	{
 		if (isdigit(argv[c]) == 0)
 		{
 		printf("Error\n");
 		return (1);
 		}
-			}
+
 	count += atoi(argv[c]);
-		}
+	count1
+	}
 	printf("%d\n", count);
 	return (0);
 }
