742f71a71eaab75ad387d13b4af8ef31b4ed4b4e
root <alerome897@gmail.com>
2023-07-05 10:04:34 -0700

    1 strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_strdup - copy a string adn returns a pointer
+ *@str: the porinter origin
+ *
+ *Return: 0
+ */
+char *_strdup(char *str)
+{
+	int size = strlen(str);
+	char *p = (char *)malloc(sizeof(char) * size + 1);
+	char *copy;
+
+	if (p == NULL)
+		return (NULL);
+copy = p;
+while (*str)
+{
+	*copy = *str;
+	copy++;
+	str++;
+}
+return (p);
+}
+
