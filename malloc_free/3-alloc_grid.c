ff6ce36eb5784d8cfb51f02371c52c3fa5bb6f02
root <alerome897@gmail.com>
2023-07-06 08:39:12 -0700

    3-alloc_grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * print_chessboard - gives a pointer to a d2 array
+ * @width: largo
+ * @height: ancho
+ * Return: void
+ */
+#include <stdio.h>
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int row;
+	int col;
+
+  if (width <= 0 || height <= 0)
+    return (NULL);
+
+  p = (int **)malloc(sizeof(int *) * height);
+
+if (p == NULL)  /** declaro espacio para las filas*/
+{	
+	return (NULL);
+}
+	for (row = 0; row < height; row++)
+	{
+	p[row] = (int *)malloc(sizeof(int *) * width);
+	if (p[row] == NULL)
+		return (NULL);
+
+	}
+	for (row = 0; row < height; row++)
+	{
+		for (col = 0; col < width; col++)
+		{
+			p[row][col] = 0;
+		}
+
+	}
+	return (p);
+}
