8c599f1e559d04c866e752cdbdc5c98dd3d84865
root <alerome897@gmail.com>
2023-07-11 11:42:13 -0700

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,27 +6,40 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
- * @d: fourth member
- * Return: nothing
+ * Return: dogg
  */
-dog_t *new_dog(char *name, __attribute__((unused)) float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
- int namelen, ownlen,i;
- dog_t *dogg;
-	namelen = (strlen(name) + 1);
-	ownlen = (strlen(owner) + 1);
-	dogg = malloc(sizeof (dog_t));
+int namelen, ownlen, i;
+	dog_t *dogg;
+
+	namelen = strlen(name) + 1;
+	ownlen = strlen(owner) + 1;
+
+	dogg = malloc(sizeof(dog_t));
 	if (dogg == NULL)
-	return (NULL);
-	for (i = 0; i < namelen; i++)
-		dogg->name[i] = name[i]; /**copy the name varible*/
+	return (NULL); /**nuevo puntero*/
+
+	/**copy the name varible*/
 	dogg->name = malloc(namelen * sizeof(char));
 	if (dogg == NULL)
+	{		free(dogg);
 	return (NULL);
+	}
+	for (i = 0; i < namelen; i++)
+		dogg->name[i] = name[i];
+
+	dogg->age = age;
+
 	dogg->owner = malloc(ownlen * sizeof(char));
+
 	if (dogg == NULL)
+	{		free(dogg->name);
+		free(dogg);
 	return (NULL);
+	}
 	for (i = 0; i < ownlen; i++) /**recorre para copiar*/
 	dogg->owner[i] = owner[i]; /**copy the owner variable*/
-		return (dogg);
+
+	return (dogg);
 }
