8649bd4cd949c77984b10ef7f4a7ae3a0f9781da
root <alerome897@gmail.com>
2023-07-11 10:57:21 -0700

    4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - stores new dog information
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * @d: fourth member
+ * Return: nothing
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+ int namelen, ownlen,i;
+ dog_t *dogg;
+	namelen = ownlen = 0;
+ while(name[namelen++])
+	;
+ while(owner[ownlen++])
+	;
+	dogg = malloc(sizeof (dog_t));
+	if (dogg == NULL)
+	return (NULL);
+	for (i = 0; i < namelen; i++)
+		dogg->name[i] = name[i]; /**copy the name varible*/
+	dogg->name = malloc(namelen * sizeof(dogg->name));
+	if (dogg == NULL)
+	return (NULL);
+	dogg->owner = malloc(ownlen * sizeof(dogg->owner));
+	if (dogg == NULL)
+	return (NULL);
+	for (i = 0; i < ownlen; i++) /**recorre para copiar*/
+	dogg->owner[i] = owner[i]; /**copy the owner variable*/
+		return (dogg);
+}
