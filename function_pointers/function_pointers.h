b4c02147f7f15f24f6e3574dc81fa80a5fdf99e0
root <alerome897@gmail.com>
2023-07-12 12:26:22 -0700

    0-print_name.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
-#include <stddef.h>
-#include <stdio.h>
 #ifndef _HEADER_
 #define _HEADER_
+
+#include <stddef.h>
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
+int _putchar (char c);
 #endif
