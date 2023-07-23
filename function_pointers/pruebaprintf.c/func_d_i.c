aea5ede21a6bcbe5056139d19f2d5e85db5c3571
root <alerome897@gmail.com>
2023-07-23 00:31:43 -0700

    pruebaass

--- function_pointers/pruebaprintf.c/func_d_i.c
+++ function_pointers/pruebaprintf.c/func_d_i.c
@@ -1,6 +1,6 @@
-#include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include "main.h"
 /**
  *print_d - prints an integer in base 10
  *@args: the argumetns it receives
@@ -8,34 +8,33 @@
  */
 int print_d(va_list args)
 {
-	long int num = va_arg(args, long int);
-	char digit[20];
-	int p = 0;
-	char sign ;
+	long int num = va_arg(args, int);
+	long int digits_ = 0;
+	int div = 1;
 	int count = 0;
 
 	if (num < 0)
 	{
-	num = -num;
-	sign = 1;
-	count++;
-    }
+		_putchar('-');
+		count++;
+		num = -num;
+	}
+	if (num < 10)	
+		return (count += _putchar(num + '0'));
 
-    do {
-	digit[p++] = '0' + (num % 10);
-	num /= 10;
-	} while (num > 0);
+	digits_ = num;
 
-if (sign == 1)
-	{
-	_putchar(45);
+	while (digits_ > 9)
+	{		
+		div *= 10;
+		digits_ /= 10;
 	}
 
-    while (p > 0)
+  while (div >= 1)
 	{
-    _putchar(digit[--p]);
-	count++;
+	count += _putchar('0' + ((num / div) % 10));
+	div /= 10;
 	}
-
-	return (count);
+  return(count);
 }
+
