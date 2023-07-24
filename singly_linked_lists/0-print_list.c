deeaf9d8d6125ff0dfefd522ca9b9969402487f5
root <alerome897@gmail.com>
2023-07-24 09:24:44 -0700

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ *print_list - prints the list
+ *@h: initial node
+ *Return: size of struct
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+{
+	printf("%s, %u\n", current->str, current->len);
+	current = current->next; /** goes to the next node*/
+	count++;
+}
+	return (count);
+}
