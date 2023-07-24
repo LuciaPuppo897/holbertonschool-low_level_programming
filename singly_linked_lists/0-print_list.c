ef12e2aeec4d17a5b7d86bc847541135f1e88bfa
root <alerome897@gmail.com>
2023-07-24 10:23:15 -0700

    fixed 0-print_list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,17 +8,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	int i;
 
 	for (i = 0; h; i++)
 {
-	if (h->str != NULL)
+	if (h->str == NULL)
+	printf("[0] (nil)\n");
+
+	else
 	{
 	printf("[%u] %s\n", h->len, h->str);
-	h = h->next;	/** goes to the next node*/
 	}
-	else
-		printf("[0], nil\n");
+	h = h->next;/** goes to the next node*/
 
 }
 return (i);
