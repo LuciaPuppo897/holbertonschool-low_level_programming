d5a848598496424e100ea19b9de5751abc3c3df8
root <alerome897@gmail.com>
2023-07-24 09:44:58 -0700

    fixing errors 0-print_list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,17 +9,21 @@
 size_t print_list(const list_t *h)
 {
 	size_t i;
+
 	for (i = 0; h; i++)
 
 	while (h != NULL)
 {
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+
+	}
+	else
+	{
 	printf("%u, %s\n", h->len, h->str);
 	h = h->next; /** goes to the next node*/
-
-}
-if (h == NULL)
-{
-	printf("[0] (nil)");
+	}
 }
 return (i);
 }
