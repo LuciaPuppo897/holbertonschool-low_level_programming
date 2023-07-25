5da8ca9493c96fde14d3a27bca8153490d23a969
root <alerome897@gmail.com>
2023-07-25 12:57:31 -0700

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,26 +1,23 @@
-#include <stdio.h>
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
 /**
- *add_node_end - adds a node at the end of the list
- *@head: a pinter to the first node
- *@str: a pointer
- *Return: 0
+ *add_node_end - add new node the end
+ *@head: pointer to the first node 
+ *@str: char
+ *Return:new node
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new_node;
 
-list_t *new_node;
+	if (head == NULL || str == NULL)
+		return (NULL);
 
-if (head == NULL || str == NULL)
-	{
-	return (NULL);
-	}
-new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
-	return (NULL);
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
@@ -29,25 +26,23 @@ new_node = malloc(sizeof(list_t));
 	return (NULL);
 	}
 
-new_node->len = strlen(str);
-new_node->next = NULL; /** el ultimo */
+	new_node->len = strlen(str);
+	new_node->next = NULL;
 
-
-if (*head == NULL)
+	if (*head == NULL)
 	{
-	*head = new_node;
+		*head = new_node;
 	}
-
 	else
 	{
 		list_t *current = *head;
 
-	while (current != NULL)
-	{
-		current = current->next;
-	}
-	current->next = new_node;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
 	}
 
-return (new_node);
+	return (new_node);
 }
