a623372af19a8df4b2771ade37221436137a6385
root <alerome897@gmail.com>
2023-07-25 18:08:03 -0700

    1-dlistint_len.c

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - length of dlistint elmetns
+ * @h: head's pointer
+ *
+ * Return: number of noder
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+	node_count++;
+	h = h->next;
+    }
+
+    return (node_count);
+}
