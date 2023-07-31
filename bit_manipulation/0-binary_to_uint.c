e72b28d7693fac7df477edec423d0a991d8f52f5
root <alerome897@gmail.com>
2023-07-30 19:18:56 -0700

    0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -17,9 +17,9 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '0')
-			resu = resu << 1;
+			result = result << 1;
 		else if (b[i] == '1')
-			resu = (resu << 1) | 1;
+			result = (result << 1) | 1;
 		else
 			return (0);
 	}
