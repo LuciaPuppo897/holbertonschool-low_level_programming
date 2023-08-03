ad3a956071f83442d50c3218369bd10b78c64cfa
root <alerome897@gmail.com>
2023-08-03 00:03:10 -0700

    3-cp.c fixed

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	int byrd = 1024, bywr = 0;
 
 	if (argc != 3)
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit (97);
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
@@ -24,8 +24,7 @@ int main(int argc, char *argv[])
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
 	}
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR
-			| S_IRGRP | S_IWGRP | S_IROTH);
+	file_to = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	if (file_to == -1)
 	{
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
