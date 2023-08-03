4f819429269e811a3a478659856569bc606f1afd
root <alerome897@gmail.com>
2023-08-02 21:52:44 -0700

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,13 +13,11 @@ int main(int argc, char *argv[])
 {
 	int file_to, file_from;
 	char buffer[1024];
-	ssize_t byrd, bywr;
+	int byrd = 1024, bywr = 0;
 
 	if (argc != 3)
-	{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
-	}
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),exit(97);
+
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
@@ -32,6 +30,8 @@ int main(int argc, char *argv[])
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 	close(file_from), exit(99);
 	}
+	while (byrd == 1024)
+	{
 	byrd = read(file_from, buffer, 1024);
 	if (byrd == -1)
 	{
@@ -42,12 +42,10 @@ int main(int argc, char *argv[])
 	if (bywr < byrd)
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	if (close(file_from) == -1)
-	{
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 	}
 	if (close(file_to) == -1)
-	{
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-	}
+	
 	return (0);
 }
