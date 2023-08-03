572c88885c15d2e2ba1ae444691d90aacedcd38a
root <alerome897@gmail.com>
2023-08-03 10:02:03 -0700

    3-cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	int file_to, file_from;
 	char buffer[1024];
-	int byrd = 1024, bywr = 0;
+	ssize_t byrd, bywr;
 
 	if (argc != 3)
 	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 	close(file_from), exit(99);
 	}
-	while (byrd == 1024)
+	while (byrd)
 	{
 	byrd = read(file_from, buffer, 1024);
 	if (byrd == -1)
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 	exit(98);
 	}
 	bywr = write(file_to, buffer, byrd);
-	if (bywr == -1)
+	if (bywr == -1 || bywr != byrd)
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	if (close(file_from) == -1)
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
