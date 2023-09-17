243bd7b84e85c5c6cd90fdf7464798b1514ea76a
root <alerome897@gmail.com>
2023-09-16 18:34:07 -0700

    fixed error in binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -12,7 +12,7 @@ int binary_search(int *array, size_t size, int value)
 	int i;
 	int left = 0;
 	int right = (int)size - 1;
-    
+
 	if (array == NULL || size == 0)
     {
     return (-1);
@@ -24,24 +24,26 @@ int binary_search(int *array, size_t size, int value)
 
     printf("Searching in array: ");
     for (i = left; i <= right; i++)
-        {
+		{
 			printf("%d", array[i]);
-        }
-        printf("\n");
+		if (i < right)
+		printf(", ");
+		}
+		printf("\n");
 
-        if (array[mid] == value)
-        {
-            printf("Found %d at index %d.\n", value, mid);
-            return (mid);
-        }
-        else if (array[mid] < value)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
+		if (array[mid] == value)
+		{
+			printf("Found %d at index %d.\n", value, mid);
+		return (mid);
+		}
+		else if (array[mid] < value)
+		{
+		left = mid + 1;
+		}
+		else
+		{
+		right = mid - 1;
+		}
     }
     return (-1);
 }
